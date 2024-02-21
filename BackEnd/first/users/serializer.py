7115b24b8450ef9db57d3f7100576ad29b0caa81
abboutah <abboutah@student.1337.ma>
2024-02-21 14:10:32 +0100

    cancel commits

--- BackEnd/first/users/serializer.py
+++
@@ -1,8 +0,0 @@
-# from rest_framework import serializers
-# from .models import api
-
-
-# class apiSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model=api
-#         fields=('name','description')
